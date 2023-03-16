dcc1f900678e20206507ffc8ba7769dafbf5cb09
AMGHNOUSS <redouane.amghnouss@edu.uiz.ac.ma>
2023-03-16 15:05:52 -0700

    Create file 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,1 +1,12 @@
-#!/bin/bash
+#include <stdio.h>
+
+/**
+ * * main - Entry point
+ *  *
+ *   * Return: 0
+ *    */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
