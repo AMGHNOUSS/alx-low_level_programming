dfe7a1d72a912e1d8ee7e423d4c78f4b54253d0b
AMGHNOUSS <redouane.amghnouss@edu.uiz.ac.ma>
2023-03-17 13:15:41 -0700

    Create file 1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -21,7 +21,7 @@ int main(void)
 	else if (ld == 0)
 	{
 		printf("Last digit of %d is %d and is 0\n", n, ld);
-	}	
+	}
 	else
 	{
 		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, ld);
