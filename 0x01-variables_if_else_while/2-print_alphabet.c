ca2b035d2daff0f162eb5fd8c8af6ff8976da2c6
AMGHNOUSS <redouane.amghnouss@edu.uiz.ac.ma>
2023-03-17 13:28:03 -0700

    Create file 2-print_alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+* main - Print the alphabet in lowercase
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	for (e = 97;e < 123;e++)
+	{
+		putchar("%c", e);
+	}
+	return (0);
+}
+
