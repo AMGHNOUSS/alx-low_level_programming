c4c855704e1a24ae8583017a863b18df7b569df4
AMGHNOUSS <redouane.amghnouss@edu.uiz.ac.ma>
2023-03-17 13:02:18 -0700

    Create file 1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -2,7 +2,7 @@
 #include <time.h>
 #include<stdio.h>
 /**
- * main : Entry point
+ * main : Print the last digit of the random number
  *
  * Return : Always 0 (Success)
  */
@@ -14,7 +14,6 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
         ld = n % 10;
-
 	if (ld > 5)
 	{
 		printf("Last digit of %d is %d and is greater than 5\n", n, ld);
