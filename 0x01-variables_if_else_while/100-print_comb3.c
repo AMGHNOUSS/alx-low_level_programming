18ac479aeced7c62829bd96f405a1786fc155d53
AMGHNOUSS <redouane.amghnouss@edu.uiz.ac.ma>
2023-03-17 16:04:47 -0700

    Create file 100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -11,11 +11,11 @@ int main(void)
 
 	for (i = '0'; i <= '8'; i++)
 	{
-		for (j = i+1; j<='9'; j++)
+		for (j = i+1; j <= '9'; j++)
 		{ 
 			putchar(i);
 			putchar(j);
-			if( i== '8' && j== '9')
+			if( i == '8' && j == '9')
 				break;
 			putchar(',');
 			putchar(' ');
