3204c89e9293552593e9473c3daf333a0b9748ae
AMGHNOUSS <redouane.amghnouss@edu.uiz.ac.ma>
2023-03-17 14:41:12 -0700

    Create file 6-print_numbers.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+* main - Print the number
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int c;
+
+	for (c = '0'; c <= '9'; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
+
