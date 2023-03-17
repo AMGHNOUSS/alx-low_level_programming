e638f5b7cb23a30e223552ed75bcba7460029ffc
AMGHNOUSS <redouane.amghnouss@edu.uiz.ac.ma>
2023-03-17 15:34:13 -0700

    Create file 8-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -12,9 +12,11 @@ int main(void)
 	for (c = '0'; c <= '9'; c++)
 	{
 		putchar(c);
-		putchar(', ');
+		if (c == '9');
+			break;
+		putchar(',');
+		putchar(' ');
 	}
-	putchar('$');
 	putchar('\n');
 	return (0);
 }
