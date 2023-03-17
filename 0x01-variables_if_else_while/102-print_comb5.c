ac90c89a5c76daeafae41baafee633cc048ddd69
AMGHNOUSS <redouane.amghnouss@edu.uiz.ac.ma>
2023-03-17 16:46:14 -0700

    Create file 102-print_comb5.c

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+* main - Print all possible different combinations of two two-digit
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int i, j, k, x;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		for (j = '0'; j <= '8'; j++)
+		{
+			for (k = i + 1; k <= '9'; k++)
+			{
+				for (x = j + 1; x <= '9'; x++ )
+				{
+					putchar(i);
+					putchar(j);
+					putchar(k);
+					putchar(x);
+					if (i == '9' && j == '8'  && k == '9'  && x == '9')
+						break;
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
+
