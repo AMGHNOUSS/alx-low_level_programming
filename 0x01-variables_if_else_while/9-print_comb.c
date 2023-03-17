e47922910ea03ea99bd45b905a3818f6d96dc703
AMGHNOUSS <redouane.amghnouss@edu.uiz.ac.ma>
2023-03-17 15:25:49 -0700

    Create file 8-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+* main - Print all possible cominations of single-digit numbers
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int c;
+
+	for (c = '0'; c <= '9'; c++)
+	{
+		putchar(c', ');
+	}
+	putchar('$\n');
+	return (0);
+}
