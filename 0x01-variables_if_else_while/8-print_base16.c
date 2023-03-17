2f491feed3b0c556183ab4cd9c30e1cb307e4e0f
AMGHNOUSS <redouane.amghnouss@edu.uiz.ac.ma>
2023-03-17 15:19:27 -0700

    Create file 8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+* main - Print the alphabet in lowercase in reverse
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int c;
+
+	for (c = '0'; c <= '9'; c++)
+	{
+		putchar(c);
+	}
+	for (c= 'a'; c <='f'; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
