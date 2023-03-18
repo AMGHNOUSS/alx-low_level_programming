e99dc164024061f101510379cd38addbbec98119
AMGHNOUSS <redouane.amghnouss@edu.uiz.ac.ma>
2023-03-18 05:58:44 -0700

    Create file 120-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -17,7 +17,7 @@ int main(void)
 			{
 				for (x = '0'; x <= '9'; x++)
 				{
-					if ((i * 10) + j < (k * 10) + x)
+					if (i * 10 + j < k * 10 + x)
 					{
 						putchar(i);
 						putchar(j);
