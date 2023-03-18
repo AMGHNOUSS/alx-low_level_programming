cb869a77ae03898b3fa6806764420a210d78cd95
AMGHNOUSS <redouane.amghnouss@edu.uiz.ac.ma>
2023-03-18 08:16:29 -0700

    Create file 102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -25,9 +25,7 @@ int main(void)
 						putchar(k);
 						putchar(x);
 						if (i == '9' && j == '8' && k == '9' && x == '9')
-						{
 							break;
-						}
 						putchar(',');
 						putchar(' ');
 					}
