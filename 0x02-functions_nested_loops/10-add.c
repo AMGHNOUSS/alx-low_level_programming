95c43257e0c939b3d32fd6f3b92d7e11f70dee94
AMGHNOUSS <redouane.amghnouss@edu.uiz.ac.ma>
2023-03-21 13:22:27 -0700

    Create file 10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * add - Add two value
+ *@a : integer
+ *@b : integer
+ * Return: add (Success)
+ */
+int add(int a, int b)
+{
+	int res;
+
+	res = a + b ;
+	return(res);
+}
