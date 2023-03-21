994a1ba2a90272e94f409db3e958a353fe0d07e4
AMGHNOUSS <redouane.amghnouss@edu.uiz.ac.ma>
2023-03-21 10:26:38 -0700

    Create file 6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _abs - Print the absolute value of a number
+ * @n : character
+ * Return: 0 or 1 (Success)
+ */
+int _abs(int n)
+{
+	if (n > 0)
+	{
+		return (n);
+	}
+	else
+	{
+		return (-n);
+	}
+}
