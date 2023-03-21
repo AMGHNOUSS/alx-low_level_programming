41dabe10f3c063065386a65799022e744e2fa50e
AMGHNOUSS <redouane.amghnouss@edu.uiz.ac.ma>
2023-03-21 11:45:11 -0700

    Create file 9-times_table.c

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * times_table - Print the 9 times table
+ *
+ * Return:  (Success)
+ */
+void times_table(void)
+{
+	int i, j, res;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			res = i * j;
+			_putchar(res + '0');
+			_putchar(',');
+			_putchar(' ');
+			_putchar(' ');
+		}
+		_putchar('\n');
+	}
+i}
