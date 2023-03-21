3f69f3d2367fa658952cb13e0d0bc079fbd94267
AMGHNOUSS <redouane.amghnouss@edu.uiz.ac.ma>
2023-03-21 13:57:41 -0700

    Create file 11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,14 +10,18 @@ void print_to_98(int n)
 	int i;
 
 	if (n == 98)
+	{
 		printf("%d", n);
+	}
 	else if (n > 98)
 	{
 		for (i = n; i >= 98; i--)
 			printf("%d", i);
 	}
-	else 
+	else	
+	{
 		for (i = n; i <= 98; i++)
 			printf("%d", i);
+	}
 	_putchar('\n');
 }
