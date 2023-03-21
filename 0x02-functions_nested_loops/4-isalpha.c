cd7547582bf8bc6c206e601a072befe3da6168f7
AMGHNOUSS <redouane.amghnouss@edu.uiz.ac.ma>
2023-03-21 10:08:33 -0700

    Create file 4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _isalpha - Check for lowercase or uppercase
+ * @c : character
+ * Return: 0 or 1 (Success)
+ */
+int _isalpha(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	else
+		return (0);
+}
