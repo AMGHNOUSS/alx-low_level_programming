82e8e834b7543a5852778d3a3bf4d549a3477a00
AMGHNOUSS <redouane.amghnouss@edu.uiz.ac.ma>
2023-03-21 12:52:13 -0700

    Create file 9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -18,20 +18,18 @@ void times_table(void)
 				_putchar(res + '0');
 				_putchar(',');
 				_putchar(' ');
-				if (res > 9)
-				{
-					_putchar(res/10 + '0');
-					_putchar(res%10 + '0');
-				}
 			}
 			else if (j == 9)
 			{
-				_putchar(' ');
-				_putchar(res + '0');
-				if (res > 9)
+				if(res > 9)
 				{
-					_putchar(res/10 + '0');
-					_putchar(res%10 + '0');
+				_putchar(res/10 + '0');
+				_putchar(res%10 + '0');
+				}
+				else
+				{
+					_putchar(' ');
+					_putchar(res);
 				}
 			}
 			else 
