9a820f3b295507b3e0497f250a220411e5ce38db
AMGHNOUSS <redouane.amghnouss@edu.uiz.ac.ma>
2023-03-21 11:02:52 -0700

    Create file 8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -16,11 +16,11 @@ void jack_bauer(void)
 			{
 				for (x = 0; x <= 9; x++)
 				{
-					_putchar(i);
-					_putchar(j);
+					_putchar(i + '0');
+					_putchar(j + '0');
 					_putchar(':');
-					_putchar(k);
-					_putchar(x);
+					_putchar(k + '0');
+					_putchar(x + '0');
 					_putchar('\n');
 					if (i == 2 && j == 3 && j == 5 && x == 9)
 					break;
