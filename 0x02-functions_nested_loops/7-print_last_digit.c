77fcba754f52c40fb6b375af0cf5a30c53578d91
AMGHNOUSS <redouane.amghnouss@edu.uiz.ac.ma>
2023-03-21 10:32:49 -0700

    Create file 7-print_last_digit.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_last_digit - Print the last digit of a number
+ * @n : character
+ * Return: 0 to 9 (Success)
+ */
+int print_last_digit(int n)
+{
+	int res;
+
+	res = n % 10;
+	return (res);
+}
