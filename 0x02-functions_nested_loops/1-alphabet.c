10733eed7383bd118b46008b1552213cce8030af
AMGHNOUSS <redouane.amghnouss@edu.uiz.ac.ma>
2023-03-21 09:06:09 -0700

    Create file 1-alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /**
-* main - Print _putchar
-*
-* Return: Always 0 (Success)
-*/
-void print_alphabet()
+ * main - Print _putchar
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet(void)
 {
 	int c;
 	for (c = 'a'; c <= 'z'; c++)
