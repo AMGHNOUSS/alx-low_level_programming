c4dff2954a7cbd940c54cb256ab16325a024519f
AMGHNOUSS <redouane.amghnouss@edu.uiz.ac.ma>
2023-03-21 10:10:18 -0700

    Create file 4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -8,9 +8,7 @@ int _isalpha(int c)
 {
 	if (c >= 'a' && c <= 'z')
 		return (1);
-	else
-		return (0);
-	if (c >= 'A' && c <= 'Z')
+	else if (c >= 'A' && c <= 'Z')
 		return (1);
 	else
 		return (0);
