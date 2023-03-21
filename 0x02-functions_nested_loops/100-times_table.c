6be2dbb3b6bea35a4cbe5a12916cfa0929d72a38
AMGHNOUSS <redouane.amghnouss@edu.uiz.ac.ma>
2023-03-21 14:51:28 -0700

    Create file 100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -12,12 +12,12 @@ void print_times_table(int n)
 	{
 	for (i = 0; i <= n; i++)
 	{
-		_putchar(48);
+		_putchar('0');
 		for (j = 1; j <= n; j++)
 		{
-			_putchar(44);
-			_putchar(32);
-			_putchar(32);
+			_putchar(',');
+			_putchar(' ');
+			_putchar(' ');
 			res = i * j;
 			if (res >= 100)
 			{
@@ -25,14 +25,14 @@ void print_times_table(int n)
 				_putchar((res % 100 ) / 10 + '0');
 				_putchar(res % 10 + '0');
 			}
-			if (res >= 10)
+			if (res >= 10 && res <= 99)
 			{
 				_putchar(res / 10 + '0');
 				_putchar(res % 10 + '0');
 			}
 			else
 			{
-				_putchar(32);
+				_putchar(' ');
 				_putchar(res + 48);
 			}
 		}
