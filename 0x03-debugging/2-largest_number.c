2d37ae6b1abe70ae60b88058e790ecd031b3b52f
AMGHNOUSS <redouane.amghnouss@edu.uiz.ac.ma>
2023-03-22 13:48:06 -0700

    Create file 2-largest_number.c and change main.h

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -1,13 +1,13 @@
 #include "main.h"
 /**
  * largest_number - returns the largest of 3 numbers
- * @c: first integer
+ * @a: first integer
  * @b: second integer
  * @c: third integer
  * Return: largest number
  */
 
-int largest_number(int a, int b,int c)
+int largest_number(int a, int b, int c)
 {
 	int largest;
 
