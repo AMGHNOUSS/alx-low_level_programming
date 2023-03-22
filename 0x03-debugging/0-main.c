0c663e09dcaa11f5d41bfd1d83cc362f7b5e1132
AMGHNOUSS <redouane.amghnouss@edu.uiz.ac.ma>
2023-03-22 12:38:48 -0700

    Create file 0-main.c and main.h

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - test function positive or negative with 0.
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
