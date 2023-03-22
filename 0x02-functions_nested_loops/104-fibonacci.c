93390b8f200970fbfe3561b9503599a249f0dcca
AMGHNOUSS <redouane.amghnouss@edu.uiz.ac.ma>
2023-03-22 15:14:32 -0700

    finish The project

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -6,25 +6,39 @@
  */
 int main(void)
 {
-	double i, j, chng;
-	int comp;
+	int inc;
+	unsigned long n1 = 0, n2 = 1, n3;
+	unsigned long n1_h1, n1_h2, n2_h1, n2_h2;
+	unsigned long h1, h2;
 
-	comp = 1;
-	i = 1;
-	j = 2;
-	printf("%.0f, ", i);
-	while (comp <= 98)
+	for (inc = 0; inc < 92; inc++)
 	{
-		comp++;
-		chng = i + j;
-		i = j;
-		j = chng;
-		if (comp == 98)
+		n3 = n1 + n2;
+		printf("%lu, ", n3);
+		n1 = n2;
+		n2 = n3;
+	}
+	n1_h1 = n1 / 10000000000;
+	n2_h1 = n2 / 10000000000;
+	n1_h2 = n1 % 10000000000;
+	n2_h2 = n2 % 10000000000;
+	for (inc = 93; inc < 99; inc++)
+	{
+		h1 = n1_h1 + n2_h1;
+		h2 = n1_h2 + n2_h2;
+		if ((n1_h2 + n2_h2) > 9999999999)
 		{
-			printf("%.0f", i);
-			break;
+			h1 += 1;
+			h2 %= 10000000000;
 		}
-		printf("%.0f, ", i);
+		printf("%lu%lu", h1, h2);
+		if (inc != 98)
+			printf(", ");
+
+		n1_h1 = n2_h1;
+		n1_h2 = n2_h2;
+		n2_h1 = h1;
+		n2_h2 = h2;
 	}
 	printf("\n");
 	return (0);
