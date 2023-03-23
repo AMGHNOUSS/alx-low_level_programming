63dfb1626912ef0e71584976baa61b49ed4ab6fe
AMGHNOUSS <redouane.amghnouss@edu.uiz.ac.ma>
2023-03-23 08:09:54 -0700

    Create file 0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * isupper - Check uppercase character
+ * @c: integer
+ * Return: 1 or 0
+ */
+int isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+		return (1);
+	else
+		return (0);
+}
