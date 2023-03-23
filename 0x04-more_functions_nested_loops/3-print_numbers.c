e735c88d3149497b3a49b0cadb911a4309e64ab7
AMGHNOUSS <redouane.amghnouss@edu.uiz.ac.ma>
2023-03-23 08:38:11 -0700

    Create file 3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_numbers - Print number
+ * Return: Nothing
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+		_putchar(i);
+	_putchar('\n');
+}
