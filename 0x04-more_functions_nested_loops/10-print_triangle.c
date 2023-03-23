840a37fbea23d0f498825dc3af89488c6285c6f6
AMGHNOUSS <redouane.amghnouss@edu.uiz.ac.ma>
2023-03-23 10:30:08 -0700

    Create file 10-print_trriangle.c

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_triangle - Print traingle followed by new line
+ * @size: integer
+ * Return: Nothing
+ */
+void print_square(int size)
+{
+	int i, j, s;
+
+	s = size - 1; 
+	if (size > 0)
+		for (i = 1; i <= size; i++)
+		{
+			for (j = 1; j <= s; j++)
+				_putchar(' ');
+			for (j = s + 1; j <= size; j++)
+				putchar('#');
+			s--;
+			_putchar('\n');
+		}
+	else
+		_putchar('\n');
+}
