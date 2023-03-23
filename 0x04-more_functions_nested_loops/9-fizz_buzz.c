a769b71d3cd8d59cb3f9a97f48ee0d4688481965
AMGHNOUSS <redouane.amghnouss@edu.uiz.ac.ma>
2023-03-23 11:14:07 -0700

    Create file 9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,22 +1,25 @@
 #include <stdio.h>
 /**
- * main - Fizz-Buzz
- * Return : Always 0 (Success)
+ *main - Fizz-Buzz
+ *Return: Always 0 (Success)
  */
-int main()
+int main(void)
 {
 	int i;
 
 	for (i = 1; i <= 100; i++)
 	{
 		if (i % 3 == 0 && !(i % 5 == 0))
-			printf("%s", "Fizz");
+			printf("Fizz ");
 		else if (i % 5 == 0 && !(i % 3 == 0))
-			printf("%s", "Buzz");
+			printf("Buzz ");
 		else if (i % 3 == 0 && i % 5 == 0)
-			printf("%s", "FizzBuzz");
-		else
+			printf("FizzBuzz ");
+		else if (i == 100)
 			printf("%d", i);
+		else
+			printf("%d ", i);
+
 	}
 	return (0);
 }
