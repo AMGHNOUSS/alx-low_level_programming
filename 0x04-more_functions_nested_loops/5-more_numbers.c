07d56354d0d4b19c987527dd7c5095d4140f64dd
AMGHNOUSS <redouane.amghnouss@edu.uiz.ac.ma>
2023-03-23 09:01:27 -0700

    Create file 5-more_numbers.c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * more_numbers- Print number followed by a new line ten times
+ * Return: Nothing
+ */
+void more_numbers(void)
+{
+	int i, j;
+
+	for (j = 0; j <= 9; i++)
+	{
+		for (i = '0'; i <= '14'; i++)
+		{
+			if (i >= 10)
+				_putchar(i / 10);
+			_putchar(i % 10);
+		}
+		_putchar('\n');
+	}
+}
