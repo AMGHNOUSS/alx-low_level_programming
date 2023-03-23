3cb17670ff10b5d21d58d7b0b29979486f62e228
AMGHNOUSS <redouane.amghnouss@edu.uiz.ac.ma>
2023-03-23 11:30:23 -0700

    Create file 9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -10,15 +10,15 @@ int main(void)
 	for (i = 1; i <= 100; i++)
 	{
 		if (i % 3 == 0 && i % 5 == 0)
-			printf("FizzBuzz ");
+			printf("FizzBuzz");
 		else if (i % 5 == 0)
-			printf("Buzz ");
+			printf("Buzz");
 		else if (i % 3 == 0)
-			printf("Fizz ");
-		else if (i == 100)
-			printf("%d", i);
+			printf("Fizz");
 		else
-			printf("%d ", i);
+			printf("%d", i);
+		if (i != 100)
+			printf(" ");
 
 	}
 	return (0);
