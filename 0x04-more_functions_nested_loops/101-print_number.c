89e84f63a4a584770818c7ddd7bd4d6cebf92c41
AMGHNOUSS <redouane.amghnouss@edu.uiz.ac.ma>
2023-03-23 18:45:05 -0700

    Create file Finish Project

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -7,12 +7,13 @@
 void print_number(int n)
 {
 	unsigned int num = n;
+
 	if (n < 0)
 	{
 		num = -num;
 		_putchar('-');
 	}
 	if (num > 9)
-		print_number(num /10);
+		print_number(num / 10);
 	_putchar(num % 10 + '0');
 }
