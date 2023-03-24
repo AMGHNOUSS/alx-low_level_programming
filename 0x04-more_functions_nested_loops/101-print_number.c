aeb1f26ff9a46761c5f6163ed8a95a7db7b1f58f
AMGHNOUSS <redouane.amghnouss@edu.uiz.ac.ma>
2023-03-23 18:41:19 -0700

    Create file 101-print_nuber.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -6,36 +6,13 @@
  */
 void print_number(int n)
 {
-	int i, num1 = 1, count = 0, num, num2;
-
-	num = n;
-	num2 = n;
-	if (n == 0)
-		_putchar(48);
-	else
+	unsigned int num = n;
+	if (n < 0)
 	{
-		if (n < 0)
-		{
-			_putchar('-');
-			num2 = -num2;
-		}
-		while (num != 0)
-		{
-			num /= 10;
-			count++;
-		}
-		for (i = 1; i < count; i++)
-			num1 = num1 * 10;
-		for (i = 1; i <= count; i++)
-		{
-			if (num1 != 1)
-			{
-				_putchar(num2 / num1 + '0');
-				num2 = num2 % num1;
-				num1 /= 10;
-			}
-			else
-				_putchar(num2 % 10 + '0');
-		}
+		num = -num;
+		_putchar('-');
 	}
+	if (num > 9)
+		print_number(num /10);
+	_putchar(num % 10 + '0');
 }
