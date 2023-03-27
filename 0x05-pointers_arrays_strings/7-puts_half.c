dbfaf81cfc7b99aa2c45b62cbc692fb59ca55be5
AMGHNOUSS <redouane.amghnouss@edu.uiz.ac.ma>
2023-03-27 16:15:59 -0700

    Create file 7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -11,7 +11,12 @@ void puts_half(char *str)
 	int i, len;
 
 	len = strlen(str);
-	for (i = len / 2; i < len; i++)
-		printf("%c", str[i]);
+
+	if (len % 2 == 0)
+		for (i = len / 2; i < len; i++)
+			printf("%c", str[i]);
+	else
+		for (i len / 2 +1; i < len; i++)
+			printf("%c", str[i])
 	printf("\n");
 }
