c77ce495a85b131307f1a21ca7327fc67ef8dcbc
AMGHNOUSS <redouane.amghnouss@edu.uiz.ac.ma>
2023-03-27 10:13:45 -0700

    Create file 2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strlen - The lenght of a string
+ * @s: integer
+ * Return: return lenght of a string
+ */
+int _strlen(char *s)
+{
+	int c;
+
+	c = strlen(s);
+	return (c);
+}
