cb0a81bd9d0885bf376e15804d1be62a3d4e07b2
AMGHNOUSS <redouane.amghnouss@edu.uiz.ac.ma>
2023-03-27 19:39:41 -0700

    Create file 100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -8,13 +8,14 @@ int _atoi(char *s)
 {
 	int i = 1;
 	unsigned int num = 0;
+
 	do {
 		if (*s == '-');
 			i *= -1;
 		else if (*s >= '0' && *s <= '9')
-			num = num * 10 +(*s - '0');
+			num = (num * 10) + *s - '0';
 		else if (num > 0)
 			break;
-	} while (*s++)
+	} while (*s++);
 	return (num * i);
 }
