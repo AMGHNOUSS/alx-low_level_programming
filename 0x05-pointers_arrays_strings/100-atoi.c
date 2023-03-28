e326e147441feb29d3088a15844cbfb4f71d8a6e
AMGHNOUSS <redouane.amghnouss@edu.uiz.ac.ma>
2023-03-27 19:35:10 -0700

    Create file 100-atoi.c

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _atoi -  convert a string to an integer.
+ * @s: string
+ * Return: Nothing
+ */
+int _atoi(char *s)
+{
+	int i = 1;
+	unsigned int num = 0;
+	do {
+		if (*s == '-');
+			i *= -1;
+		else if (*s >= '0' && *s <= '9')
+			num = num * 10 +(*s - '0');
+		else if (num > 0)
+			break;
+	} while (*s++)
+	return (num * i);
+}
