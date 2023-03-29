cac03b92dd9a605e6de018690006dc6280a5d212
AMGHNOUSS <redouane.amghnouss@edu.uiz.ac.ma>
2023-03-29 15:44:10 -0700

    Create file 1-strncat.c

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _strncat - Concatenates two strings.
+ * @dest: string
+ * @src: string
+ * Return: string
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int cpm = 0, i = 0;
+
+	while (*(dest + cpm) != '\0')
+		cpm++;
+	for (i = 0; *(src + i) != '\0' && i < n; i++)
+		*(dest + cpm + i) = *(src + i);
+	return (dest);
+}
