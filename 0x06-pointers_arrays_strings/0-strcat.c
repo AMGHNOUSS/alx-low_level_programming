61e67ce542a0e4afe442a3e318efcf6846fbddde
AMGHNOUSS <redouane.amghnouss@edu.uiz.ac.ma>
2023-03-29 15:35:03 -0700

    Create file 0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -7,12 +7,11 @@
  */
 char *_strcat(char *dest, char *src)
 {
-	int cpm = sizeof(dest), i = 0;
+	int cpm = 0, i = 0;
 
-	while (*(src + i) != '\0')
-	{
+	while(*(dest + cpm) != '\0')
+		cpm++;
+	for (i = 0; *(src + i) != '\0'; i++)
 		*(dest + cpm + i) = *(src + i);
-		i++;
-	}
 	return (dest);
 }
