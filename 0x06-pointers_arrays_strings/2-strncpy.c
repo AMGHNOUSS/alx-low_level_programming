7d1978efccb1ba4eebe1311f3cc1c5e9ec7232f0
AMGHNOUSS <redouane.amghnouss@edu.uiz.ac.ma>
2023-03-29 16:25:06 -0700

    Create file 2-strncpy.c

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _strncpy - Copie a string
+ * @dest: string
+ * @src: string
+ * @n: int
+ * Return: string
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int cmp = 0, i = 0;
+	
+	while (*(dest + cmp) != '\0')
+		cmp++;
+	for (i = 0; *(src + i) != '\0' && i < n; i++)
+	{
+		if (i < cmp)
+			*(dest + i) = *(src + i);
+		else
+			*(dest + i) = '\0';
+	}
+	return (dest);
+}
