a7a293be6e5ba7e89a53d5c59a2395342bec87d6
AMGHNOUSS <redouane.amghnouss@edu.uiz.ac.ma>
2023-03-29 15:17:02 -0700

    Create file 0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 /**
  * _strcat - Concatenates two strings.
  * @dest: string
@@ -10,7 +9,6 @@ char *_strcat(char *dest, char *src)
 {
 	int cpm = sizeof(dest), i = 0;
 
-	printf("%d\n", cpm);
 	while (*(src + i) != '\0')
 	{
 		*(dest + cpm + i) = *(src + i);
