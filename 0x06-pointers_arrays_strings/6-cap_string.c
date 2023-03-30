83198285bfa7b40a9b485a51cb17ed44a84c1bf1
AMGHNOUSS <redouane.amghnouss@edu.uiz.ac.ma>
2023-03-29 19:57:04 -0700

    Create file 5-string_toupper.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
- * string_toupper - changes all lowercase letters of a string to uppercase.
- * @a: string
- * Return: string
+ * String_toupper - changes all lowercase letters of a string to uppercase.
+ * @a: char
+ * Return: Nothings
  */
 char *string_toupper(char *a)
 {
