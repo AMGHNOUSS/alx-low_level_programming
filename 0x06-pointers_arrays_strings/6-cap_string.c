efb81c8bb3b144c091795352f986500abb0e8f0f
AMGHNOUSS <redouane.amghnouss@edu.uiz.ac.ma>
2023-03-30 09:53:39 -0700

    Create file 6-cap_string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -6,11 +6,13 @@
  */
 char *cap_string(char *a)
 {
-	int i, j;
+	int i = 0, j;
 	char bb[] = {9, 10, 32, 33, 34, 40, 41, 44, 46, 59, 63, 123, 125};
 	char *b;
 
 	b = bb;
+	if (*(a + i) >= 97 && *(a + i) <= 122)
+		*(a + i) = *(a + i) - 32;
 	for (i = 0; *(a + i); i++)
 		for (j = 0; *(b + j); j++)
 			if (*(a + i) == *(b + j))
