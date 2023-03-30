6c13c0ef2f3d60ec444ba61d226a7f5069bdc366
AMGHNOUSS <redouane.amghnouss@edu.uiz.ac.ma>
2023-03-30 16:13:25 -0700

    Create file 101-print_number.c

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_number - Print number
+ * @n: integer
+ * Return: Nothing
+ */
+void print_number(int n)
+{
+	unsigned int num = n;
+
+	if (n < 0)
+	{
+		num = -num;
+		_putchar('-');
+	}
+	if (num > 9)
+		print_number(num / 10);
+	_putchar(num % 10 + '0');
+}
