774401e86d14309dae4838569ab0bf4b400c0f6f
AMGHNOUSS <redouane.amghnouss@edu.uiz.ac.ma>
2023-03-29 18:57:21 -0700

    Create file 3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -3,14 +3,30 @@
  * _strcmp - Copie a string
  * @s1: string
  * @s2: string
- * Return: string
+ * Return: 1 or 15 or -15
  */
 int _strcmp(char *s1, char *s2)
 {
-	if (*s1 > *s2)
-		return (15);
-	else if (*s2 > *s1)
-		return (-15);
-	else
-		return (0);
+	int i = 0, r, cmp = 0, cmp1 = 0, cmp2 = 0;
+
+	while (*(s1 + cmp) != '\0')
+		cmp1++;
+	while (*(s2 + cmp) != '\0')
+		cmp2++;
+	if (cmp1 <= cmp2 )
+		cmp1 = cmp2;
+	for (i = 0; i < cmp1; i++)
+	{
+		if (*(s1 + i) < *(s2 + i))
+		{
+			r = *(s1 + i) - *(s2 + i);
+			return (r);
+		}
+		else if (*(s1 + i) > *(s2 + i))
+		{
+			r = *(s1 + i) - *(s2 + i);
+			return (r);
+		}
+	}
+	return (0);
 }
