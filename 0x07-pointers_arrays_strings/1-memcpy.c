d11499569101d96a4437401ac5a3e7b80de96168
AMGHNOUSS <redouane.amghnouss@edu.uiz.ac.ma>
2023-04-03 14:43:11 -0700

    Create file 1-memcpy.c

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _memcpy - copies memory area.
+ * @dest: string
+ * @src: string
+ * @n: integer
+ * Return: string
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i = 0;
+
+	while (i < n)
+	{
+		*(dest + i) = *(src + i);
+		i++;
+	}
+	return (dest);
+}
