3661f154bd639f907d43b70178119e12fd9d5eb3
AMGHNOUSS <redouane.amghnouss@edu.uiz.ac.ma>
2023-04-03 15:12:58 -0700

    Create file 2-strchr.c

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _strchr - locates a character in a string.
+ * @s: string
+ * @c: string
+ * Return: string
+ */
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+
+	while (*(s + i))
+	{
+		if (*(s + i) == c)
+			return (s + i);
+		i++;
+	}
+	return ('\0');
+}
