d7ebb66eecf8e8ed163628a38177cf64959bb522
AMGHNOUSS <redouane.amghnouss@edu.uiz.ac.ma>
2023-04-03 17:59:18 -0700

    Create file 4-strpbrk.c

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -3,9 +3,18 @@
  * _strpbrk - searches a string for any of a set of bytes.
  * @s: string
  * @accept: string
- * Return: integer
+ * Return: string
  */
 char *_strpbrk(char *s, char *accept)
 {
-	
+	int i = 0, j;
+
+	while (*(s + i))
+	{
+		for (j = 0; *(accept + j); j++)
+			if (accept[j] == *(s + i))
+				return (s + i);
+		i++;
+	}
+	return ('\0');
 }
