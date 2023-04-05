92d87d70856e67c0ac44cbbcd11a6d2803ac6401
AMGHNOUSS <redouane.amghnouss@edu.uiz.ac.ma>
2023-04-05 15:42:04 -0700

    Create file 6-is_prime_recursion.c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,15 +1,29 @@
 #include "main.h"
 /**
+ * prime2 - test for number
+ * @n: Integer
+ * @x: Integer
+ * Return: Integer
+ */
+int prime2(int n, int x)
+{
+	if (n == x)
+		return (1);
+	else if (n % x == 0)
+		return (0);
+	else
+		return (prime2(n, x + 1));
+}
+/**
  * is_prime_number - Check if a number is prime.
  * @n: Integer
  * Return: Int
  */
 int is_prime_number(int n)
 {
-	if (n < 0)
+	if (n < 1)
 		return (0);
 	else if (n == 1)
-		return (1);
-	else
-		return (n / _is_prime_number(n - 1));
+		return (0);
+	return (prime2(n, 2));
 }
