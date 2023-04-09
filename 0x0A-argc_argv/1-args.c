cfc158a0e55ebec2bcb0512a846fc102a00f8cba
AMGHNOUSS <redouane.amghnouss@edu.uiz.ac.ma>
2023-04-08 22:24:59 -0700

    Create file 1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
-/*main - Print nimber of arguments
- *argc: Integer
- *argv: String
+/**
+ *main - Print nimber of arguments
+ *@argc: Integer
+ *@argv: String
  *Return: Always 0
  */
-int main(int __attribute__((unused)) argc, char __attribute__((unused)) *argv[])
+int main(int argc, char __attribute__((unused)) *argv[])
 {
-	printf("%d\n", argc);
-	return(0);
+	printf("%d\n", --argc);
+	return (0);
 }
