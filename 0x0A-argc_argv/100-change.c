b80a679e24066d935cc4bc67d906a8b9af2d582a
AMGHNOUSS <redouane.amghnouss@edu.uiz.ac.ma>
2023-04-08 23:00:30 -0700

    Create file 4-add.c

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - print the minimum number of coins.
+ * @argc: integer
+ * @argv: String
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+	return (0);
+}
