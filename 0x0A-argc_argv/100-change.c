47009f72c3426d3680f442876abbdf472f59b87e
AMGHNOUSS <redouane.amghnouss@edu.uiz.ac.ma>
2023-04-08 23:56:42 -0700

    Create file 100-change.c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -8,5 +8,33 @@
  */
 int main(int argc, char *argv[])
 {
+	int i, sum, cmp = 0;
+	int tab[5] = {25, 10, 5, 2, 1};
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+	}
+	else if (atoi(argv[1]) < 0)
+	{
+		printf("0\n");
+	}
+	else if (atoi(argv[1]) > 0)
+	{
+		sum = atoi(argv[1]);
+		while (sum != 0)
+		{
+			for (i = 0; i < 5; i++)
+			{
+				if (sum >= tab[i])
+				{
+					sum -= tab[i];
+					cmp++;
+					break;
+				}
+			}
+		}
+		printf("%d\n", cmp);
+	}
 	return (0);
 }
