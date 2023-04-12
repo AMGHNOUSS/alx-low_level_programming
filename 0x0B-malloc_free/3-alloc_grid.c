89c66575b810c0fa25c255651dd183b78206cf7f
AMGHNOUSS <redouane.amghnouss@edu.uiz.ac.ma>
2023-04-11 19:06:08 -0700

    Create file 3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -12,13 +12,23 @@ int **alloc_grid(int width, int height)
 	if (width <= 0 || height <= 0)
 		return (0);
 	p = (int **) malloc(sizeof(int *) * height);
-	for (i = 0; i < width; i++)
+	if (p)
 	{
-		p[i] = (int *) malloc(sizeof(int) * width);
-		for (j = 0; j < height; j++)
+		for (i = 0; i < height; i++)
 		{
-			p[i][j] = 0;
+			p[i] = (int *) malloc(sizeof(int) * width);
+			if (p[i])
+			{
+				for (j = 0; j < width; j++)
+				{
+					p[i][j] = 0;
+				}
+			}
 		}
+	}
+	else
+	{
+		return (0);
 	}
 	return (p);
 }
