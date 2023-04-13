713fd4643fc14b61c8261a6d2b82484989e74559
AMGHNOUSS <redouane.amghnouss@edu.uiz.ac.ma>
2023-04-13 11:43:45 -0700

    Create file 3-array_range.c

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -12,14 +12,14 @@ int *array_range(int min, int max)
 
 	if (min > max)
 		return (0);
-	p = malloc((max - min) * sizeof(int));
+	p = malloc((max - min) * sizeof(int) + 1);
 	if (!p)
 		return (0);
-	count = min;
 	for (i = 0; i < (max - min); i++)
 	{
 		*(p + i) = count;
 		count++;
 	}
+	*(p + i) = max;
 	return (p);
 }
