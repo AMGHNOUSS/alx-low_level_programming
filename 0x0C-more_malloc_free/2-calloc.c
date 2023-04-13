ee68d61509a0b33a7ae67cd2225bc9693dbcfa34
AMGHNOUSS <redouane.amghnouss@edu.uiz.ac.ma>
2023-04-13 11:10:25 -0700

    Update file 2-calloc.c

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -8,12 +8,15 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	char *p;
+	unsigned int i;
 
 	if (size == 0 || nmemb == 0)
 		return (0);
 	p = malloc(nmemb);
 	if (!p)
 		return (0);
-	*(p + nmemb) = '\0';
+	for (i = 0; i < nmemb; i++)
+		*(p + i) = '\0';
+	*(p + i) = '\0';
 	return (p);
 }
