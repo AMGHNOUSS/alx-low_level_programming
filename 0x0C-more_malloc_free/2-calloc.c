18465b9b26eb6d421b21184c48fd78fe801f0f6e
AMGHNOUSS <redouane.amghnouss@edu.uiz.ac.ma>
2023-04-13 10:45:29 -0700

    Create file 2-calloc.c

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+/**
+ * _calloc - concatenate two strings.
+ * @size: Integer
+ * @nmemb: Integer
+ * Return: Nothings
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *p;
+
+	if (size == 0 || nmemb == 0)
+		return (0);
+	p = malloc(nmemb);
+	if (!p)
+		return (0);
+	return (p);
+}
