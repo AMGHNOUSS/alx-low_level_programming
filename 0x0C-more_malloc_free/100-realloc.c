c8b5eb4d1b8ed8d4b21d1a50f931b67d53c66a2e
AMGHNOUSS <redouane.amghnouss@edu.uiz.ac.ma>
2023-04-13 16:32:44 -0700

    Create file 100-realloc.c

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,48 @@
+#include <stdlib.h>
+/**
+ * _memcpy - copy string
+ * @destination: char
+ * @source: Char
+ * @n:Integer
+ * Return: string
+ */
+void *_memcpy(void *destination, const void *source, unsigned int n)
+{
+	char *dst = (char *)destination;
+	const char *src = (const char *)source;
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		dst[i] = src[i];
+	}
+	return (destination);
+}
+/**
+ * _realloc - reallocates a memory block using malloc and free.
+ * @new_size: Integer
+ * @old_size: Integer
+ * @ptr: Void pointer
+ * Return: Tab of POintr
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	void *p;
+
+	if (new_size > old_size)
+	{
+		p = malloc(old_size + new_size);
+		_memcpy(p, ptr, old_size);
+		return (p);
+	}
+	else if (new_size == old_size)
+	{
+		return (ptr);
+	}
+	else if (new_size == 0)
+	{
+		free(ptr);
+		return (0);
+	}
+	return (ptr);
+}
