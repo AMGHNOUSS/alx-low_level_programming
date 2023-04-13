3e66253b972641dfacdca0d5190eb5c74d37799d
AMGHNOUSS <redouane.amghnouss@edu.uiz.ac.ma>
2023-04-13 16:42:23 -0700

    Create file 100-realloc.c

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -31,8 +31,14 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 
 	if (new_size > old_size)
 	{
-		p = malloc(old_size + new_size);
+		p = malloc(new_size);
 		_memcpy(p, ptr, old_size);
+		return (p);
+	}
+	else if (new_size < old_size)
+	{
+		p = malloc(new_size);
+		_memcpy(p, ptr, new_size);
 		return (p);
 	}
 	else if (new_size == old_size)
