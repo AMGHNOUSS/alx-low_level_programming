5b8b8c2c6711bf2d421a2b2c573bd7892cbd5224
AMGHNOUSS <redouane.amghnouss@edu.uiz.ac.ma>
2023-04-15 15:59:39 -0700

    Create file 1010-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -16,16 +16,16 @@ int main(int argc, char *argv[])
 		exit(98);
 	}
 	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j]; j++)
 		{
-			for (j = 0; argv[i][j]; j++)
+			if (argv[i][j] > 57 || argv[i][j] < 48)
 			{
-				if (argv[i][j] > 57 || argv[i][j] < 48)
-				{
-					printf("Error\n");
-					exit(98);
-				}
+				printf("Error\n");
+				exit(98);
 			}
 		}
+	}
 	printf("%lu\n", (atol(argv[1]) *  atol(argv[2])));
 	return (0);
 }
