3c94e76060eefc3d7c5dd50d65a730438df82a76
AMGHNOUSS <redouane.amghnouss@edu.uiz.ac.ma>
2023-04-17 06:00:00 -0700

    Create file dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef dog_h
+#define dog_h
+/**
+ * struct dog - Structure for Dog.
+ * @name: String, First member
+ * @age: Float, Second member
+ * @owner: String, Third member
+ *
+ * Descriotion: Define a new type struct dog with the name, age and owner.
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+#endif
