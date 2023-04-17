01ea743f2b5247874da75ffeb7ffa796221e970c
AMGHNOUSS <redouane.amghnouss@edu.uiz.ac.ma>
2023-04-17 09:17:06 -0700

    Create file 4-new_dog.c

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,21 @@
+#include "dog.h"
+#include <stdlib.h>
+/**
+ * new_dog - Create a new dog.
+ * name: String
+ * age: float
+ * owner: string
+ * return: dog
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *d;
+
+	d = (dog_t *)malloc(sizeof(dog_t));
+	if (!d)
+		return (0);
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+	return (d);
+}
