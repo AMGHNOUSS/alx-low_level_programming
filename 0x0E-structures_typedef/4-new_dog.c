0fcf2530e81b84bf5ce8b1fa7e2e4dcda18bb56f
AMGHNOUSS <redouane.amghnouss@edu.uiz.ac.ma>
2023-04-17 20:00:00 -0700

    ....

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -10,12 +10,33 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *d;
+	int i, len1 = 0, len2 = 0;
 
-	d = (dog_t *)malloc(sizeof(dog_t));
-	if (!d)
+	d = malloc(sizeof(dog_t));
+	if (!d || !(name) || !(owner))
+	{
+		free(d);
 		return (0);
-	d->name = name;
+	}
+	while (*(name + len1))
+		len1++;
+	while (*(owner + len2))
+		len2++;
+	d->name = malloc(len1 + 1);
+	d->owner = malloc(len2 + 1);
+	if (!(d->name) || !(d->owner))
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+		return (0);
+	}
+	for (i = 0; i < len1; i++)
+		(*d).name[i] = name[i];
+	(*d).name[i] = '\0';
+	for (i = 0; i < len2; i++)
+		(*d).owner[i] = owner[i];
+	(*d).owner[i] = '\0';
 	d->age = age;
-	d->owner = owner;
 	return (d);
 }
