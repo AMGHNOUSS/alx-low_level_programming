96be191ac6f68a64d9d25602cbe26303ff971bf5
AMGHNOUSS <redouane.amghnouss@edu.uiz.ac.ma>
2023-04-17 19:32:28 -0700

    Create file 5-free_dog.c

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,4 +1,5 @@
 #include "dog.h"
+#include <stdlib.h>
 /**
  * free_dog - Free dog.
  * @d : Type of d
@@ -7,9 +8,9 @@
 void free_dog(dog_t *d)
 {
 
-	if (d)
+	if (!d)
 		return;
 	free(d->name);
 	free(d->owner);
-	free(d->age);
+	free(d);
 }
