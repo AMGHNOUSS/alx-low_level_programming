b5a1c8ebb5e9f6a57dc56d71be4f4d4944e3b19d
AMGHNOUSS <redouane.amghnouss@edu.uiz.ac.ma>
2023-04-17 18:12:33 -0700

    Update file 2-print_dog.c

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -15,7 +15,7 @@ void print_dog(struct dog *d)
 		printf("Name: %s\n", (*d).name);
 	if ((*d).age < 0)
 		printf("Age: (nil)\n");
-	elsei
+	else
 		printf("Age: %f\n", (*d).age);
 	if ((*d).owner == 0)
 		printf("Owner: (nil)\n");
