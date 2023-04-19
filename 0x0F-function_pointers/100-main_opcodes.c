470effb3726d9f1d3a2c7e746466a8c94b7a84b8
AMGHNOUSS <redouane.amghnouss@edu.uiz.ac.ma>
2023-04-19 16:32:03 -0700

    Create file 100-main_opcode.c

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Print the opcodes of its own main function.
+ * @argc: Number of arguments
+ * @argv: Arguments
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+	int a, i;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+	a = atoi(argv[1]);
+	if (a < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+	for (i = 0; i < a; i++)
+	{
+		printf("%.2hhx", argv[1][i]);
+		if (i == a - 1)
+			break;
+		printf(" ");
+	}
+	printf("\n");
+	return (0);
+}
