5c4b011a0a1aef03ed5cbffcc74baea5d8320cbc
AMGHNOUSS <redouane.amghnouss@edu.uiz.ac.ma>
2023-04-19 08:21:26 -0700

    Create file 1-array_iterator.c

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -11,10 +11,10 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t i;
 
-	if (size <= 0)
+	if (!(array))
 		return;
 	for (i = 0; i < size; i++)
 	{
-		action(array[i]);
+		return (action(array[i]));
 	}
 }
