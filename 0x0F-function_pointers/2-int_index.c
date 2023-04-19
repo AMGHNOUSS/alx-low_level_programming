c4903af00134b7624cbfc23656c2128bb99924b2
AMGHNOUSS <redouane.amghnouss@edu.uiz.ac.ma>
2023-04-19 08:38:16 -0700

    Create file 2-int_index.c

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -11,11 +11,11 @@ int int_index(int *array, int size, int (*cmp)(int))
 	int i = -1;
 
 	if (!array || !cmp)
-		return (0);
+		return (-1);
 	for (i = 0; i < size; i++)
 	{
 		if (cmp(array[i]))
 			return (i);
 	}
-	return (i);
+	return (0);
 }
