1e28e54e2fe2257dabdeca32e0de01cab8ed8346
AMGHNOUSS <redouane.amghnouss@edu.uiz.ac.ma>
2023-04-27 11:44:10 -0700

    Printing before main

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include "lists.h"
+/**
+ * before_main - Print message befare main
+ * Return: Nothings
+ */
+void before_main(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
