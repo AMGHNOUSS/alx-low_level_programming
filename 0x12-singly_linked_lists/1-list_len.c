60783b72f49a79cf5df2bf0d51a5c66025e9775f
AMGHNOUSS <redouane.amghnouss@edu.uiz.ac.ma>
2023-04-27 09:17:57 -0700

    The number node

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -7,6 +7,7 @@
 size_t list_len(const list_t *h)
 {
 	int i = 0;
+
 	while (h)
 		h = h->next, i++;
 	return (i);
---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+/**
+ * add_node - Add a new node at the beginning of a list.
+ * @head: Singly linked list
+ * @str: String
+ * Return: Singly Linked list
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+
+}
