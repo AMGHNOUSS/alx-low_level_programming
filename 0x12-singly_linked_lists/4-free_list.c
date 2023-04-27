1b24e93ca9391f646953dceb232facd5bec2c3f5
AMGHNOUSS <redouane.amghnouss@edu.uiz.ac.ma>
2023-04-27 11:33:08 -0700

    Free all lists

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * free_list - Free list a list_t list.
+ * @head: Singly linked list
+ * Return: Nothings
+ */
+void free_list(list_t *head)
+{
+	list_t *p;
+
+	while (head)
+	{
+		p = head->next;
+		free(head->str);
+		free(head);
+		head = p;
+	}
+	free(p);
+}
