cdaf995661112fba65854b924519a6f7c5993c4b
AMGHNOUSS <redouane.amghnouss@edu.uiz.ac.ma>
2023-04-27 09:11:52 -0700

    The number node

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+/**
+ * list_len - Return the number of elements in a linked list_t list.
+ * @h: Singly linked list
+ * Return: the number of node
+ */
+size_t list_len(const list_t *h)
+{
+	int i = 0;
+	while (h)
+		h = h->next, i++;
+	return (i);
+}
