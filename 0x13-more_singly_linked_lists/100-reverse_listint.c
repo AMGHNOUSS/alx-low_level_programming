1edcf6fa34457b2492b7a68249602aec8a2ca7ff
AMGHNOUSS <redouane.amghnouss@edu.uiz.ac.ma>
2023-05-02 03:04:02 -0700

    Create File 100-reverse_listint.c

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+/**
+ * reverse_listint - Reverse a listint linked list.
+ * @head: Singly linked list
+ * Return: Integer
+ */
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *h, *before_h = NULL;
+
+	if (!head)
+		return (0);
+	if (!((*head)->next))
+		return (*head);
+	while (*head)
+	{
+		h = (*head)->next;
+		(*head)->next = before_h;
+		before_h = *head;
+		*head = h;
+	}
+	*head = before_h;
+	return (*head);
+}
