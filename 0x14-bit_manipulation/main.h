65cfa6bcd58592cc09d2a91060f12ec346cb218c
AMGHNOUSS <redouane.amghnouss@edu.uiz.ac.ma>
2023-05-04 14:07:08 -0700

    Update file main.h

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -13,5 +13,6 @@ int set_bit(unsigned long int *n, unsigned int index);
 int get_bit(unsigned long int n, unsigned int index);
 void print_binary(unsigned long int n);
 int _putchar(char c);
+int get_endianness(void);
 
 #endif
