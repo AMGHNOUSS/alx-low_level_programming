798bdd43003ba2ca33a33628cc8450453abe8191
AMGHNOUSS <redouane.amghnouss@edu.uiz.ac.ma>
2023-05-04 13:19:24 -0700

    Create FIle 5-flip_bits.c

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * flip_bits - Flip to another number
+ * @n: Integer
+ * @m: Integer
+ * Return: Integer
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	int a = n ^ m;
+	unsigned int i = 0;
+
+	while (a != 0)
+	{
+		if (a & 1)
+			i++;
+		a = (a >> 1);
+	}
+	return (i);
+}
