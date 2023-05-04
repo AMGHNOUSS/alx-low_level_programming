69e3c2a9a5c8c78c8150c99a47c54aefc5dc63e5
AMGHNOUSS <redouane.amghnouss@edu.uiz.ac.ma>
2023-05-04 14:04:49 -0700

    Create File 100-get_endianness.c

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -1,1 +1,14 @@
 #include "main.c"
+/**
+ * get_endianness - checks whether a machine is big endian or little.
+ * Return: 1 little endian or 0.
+ */
+int get_endianness(void)
+{
+	unsigned int test = 1;
+	char *endian = (char *)&test;
+
+	if (*endian)
+		return (1);
+	return (0);
+}
