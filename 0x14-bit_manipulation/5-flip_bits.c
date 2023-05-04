f041540b2f58bc08c48cccb8e8d14d96bcc51c7d
AMGHNOUSS <redouane.amghnouss@edu.uiz.ac.ma>
2023-05-04 13:38:46 -0700

    Update File 5-flip_bits.c

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -7,14 +7,14 @@
  */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-	int a = n ^ m;
+	unsigned long int a = n ^ m;
 	unsigned int i = 0;
 
 	while (a != 0)
 	{
 		if (a & 1)
 			i++;
-		a = (a >> 1);
+		a = a >> 1;
 	}
 	return (i);
 }
