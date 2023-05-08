27a2cad853e0f75f71e222bc5eebd7c38317db02
AMGHNOUSS <redouane.amghnouss@edu.uiz.ac.ma>
2023-05-08 13:02:41 -0700

    Create FIle 0-read_textfile.c

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * read_textfile - read a text file and prints it to the POSIX standard output
+ * @filename: String
+ * @letters: NUmber
+ * Return: NUmber
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	char *c;
+	ssize_t sz, wr;
+
+	fd = open(filename, O_RDONLY);
+	if (fd < 0)
+		return (0);
+	c = (char *)malloc(letters);
+	sz = read(fd, c, letters);
+	if (sz == 0)
+		return (0);
+	wr = write(1, c, sz);
+	close(fd);
+	free(c);
+	return (wr);
+}
