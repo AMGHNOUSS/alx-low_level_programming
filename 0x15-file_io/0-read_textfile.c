43dc8fd82a46ad9579890084dc8701f8b0fcfe36
AMGHNOUSS <redouane.amghnouss@edu.uiz.ac.ma>
2023-05-08 13:05:32 -0700

    Update FIle 0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -19,6 +19,8 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (sz == 0)
 		return (0);
 	wr = write(1, c, sz);
+	if (wr == 0)
+		return (0);
 	close(fd);
 	free(c);
 	return (wr);
