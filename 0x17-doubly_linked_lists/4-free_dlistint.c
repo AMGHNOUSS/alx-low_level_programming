0ecd1ef52e8427273cce0778ad8a49059b8aff1b
AMGHNOUSS <redouane.amghnouss@edu.uiz.ac.ma>
2023-06-16 00:53:33 +0100

    Create File 4-free_dlistint.c

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+/**
+ * free_dlistint - free a dlistint_t list.
+ * @head: pointer
+ * Retunr: void
+ */
+
+void free_dlistint(dlistint_t *head)
+{
+	while (head != NULL)
+	{
+		dlistint_t *next = head->next;
+		free(head);
+		head = next;
+	}
+}
