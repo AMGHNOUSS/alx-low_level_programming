99fa88bb185d25aaf8d5c5b99baad0ca74e1a7cc
AMGHNOUSS <redouane.amghnouss@edu.uiz.ac.ma>
2023-06-19 10:48:02 -0700

    tasks

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -2,12 +2,14 @@
 /**
  * free_dlistint - free a dlistint_t list.
  * @head: pointer
+ * Return: no return.
  */
 void free_dlistint(dlistint_t *head)
 {
 	while (head != NULL)
 	{
 		dlistint_t *next = head->next;
+
 		free(head);
 		head = next;
 	}
