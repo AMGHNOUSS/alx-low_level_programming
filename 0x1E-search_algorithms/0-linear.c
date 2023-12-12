f81d283c7e5e1bcda9f0cc89387192a61b162383
AMGHNOUSS <redouane.amghnouss@edu.uiz.ac.ma>
2023-12-12 17:35:37 +0100

    update code

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -9,13 +9,13 @@
  */
 int linear_search(int *array, size_t size, int value)
 {
+	if (array == NULL)
+		return (-1);
 	for (size_t i = 0; i < size; i++)
 	{
 		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
 		if (value == array[i])
-		{
 			return (i);
-		}
 	}
 	return (-1);
 }
