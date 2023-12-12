13a7ff87c6709fa4b70ad5d8b6cd9aa2e52bea77
AMGHNOUSS <redouane.amghnouss@edu.uiz.ac.ma>
2023-12-12 19:13:17 +0100

    Update code Of file 0-linear.c

---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,21 @@
+#include "search_algos.h"
+/**
+ * binary_search - searches for a value in a sorted array of integers
+ * using the Binary search algorithm.
+ *
+ * @array: array of integers
+ * @size: number of Integers
+ * @value: number searhing
+ *
+ * Return: -1 or index of value.
+ * */
+
+int binary_search(int *array, size_t size, int value)
+{
+	if (*(array) == NULL)
+		return (-1);
+	for (size_t i = 0; i < size; i++)
+	{
+		printf("Searching in array: ");
+	}
+}
