4074bd0a777a397cb044e277db50e06b3ce5193d
AMGHNOUSS <redouane.amghnouss@edu.uiz.ac.ma>
2023-12-12 20:42:16 +0100

    Update The code of File 1-binary.c

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -6,16 +6,31 @@
  * @array: array of integers
  * @size: number of Integers
  * @value: number searhing
- *
  * Return: -1 or index of value.
- * */
+ */
 
 int binary_search(int *array, size_t size, int value)
 {
-	if (*(array) == NULL)
+	int i;
+	int r = (int)size - 1, l = 0, m;
+
+	if (array == NULL)
 		return (-1);
-	for (size_t i = 0; i < size; i++)
+	while (l <= r)
 	{
 		printf("Searching in array: ");
+		for (i = l; i < r; i++)
+		{
+			printf("%d, ", array[i]);
+		}
+		printf("%d\n", array[i]);
+		m = l + (r - l) / 2;
+		if (array[m] == value)
+			return (m);
+		if (array[m] < value)
+			l = m + 1;
+		else
+			r = m - 1;
 	}
+	return (-1);
 }
