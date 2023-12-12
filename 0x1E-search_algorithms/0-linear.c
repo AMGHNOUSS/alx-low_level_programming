f671b22eb3fbd7d30d8f869cbe555861c711d900
AMGHNOUSS <redouane.amghnouss@edu.uiz.ac.ma>
2023-12-12 15:00:03 +0100

    Create File .gitignore

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,13 @@
+#include "search_algos.h"
+/**
+ * linear_search - Search for a value in an array of integers
+ * using the Linear search algorithm
+ * @array: Pointer of integer
+ * @size: size of array
+ * @value: number searching
+ * Return: NUll or index of value
+ */
+int linear_search(int *array, size_t size, int value)
+{
+	return (-1);
+}
