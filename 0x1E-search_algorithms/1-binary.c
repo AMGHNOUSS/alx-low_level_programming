7db33dde5990bc3c3adb06e5db1d081ec59eab88
AMGHNOUSS <redouane.amghnouss@edu.uiz.ac.ma>
2024-03-11 18:40:44 +0000

    Create a file 1-binary.c

---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,34 @@
+#include "search_algos.h"
+
+/**
+ * binary_search - searches for a value in an array using the Linear
+ * @array: list of integrs.
+ * @size: lenght of list.
+ * @value: value that i want to found.
+ * Return: Integer or NULL
+ */
+int binary_search(int *array, size_t size, int value)
+{
+	size_t i, L, R, m;
+
+	if (array == NULL)
+		return (-1);
+	L = 0;
+	R = size - 1;
+	
+	while (L <= R)
+	{
+		printf("Searching in array: ");
+		for (i = L; i < R; i++)
+			printf("%d, ", array[i]);
+		printf("%d\n", array[i]);
+		m = L + (R - L) / 2;
+		if (array[m] == value)
+			return (m);
+		if (array[m] > value)
+			R = m - 1;
+		else
+			L = m + 1;
+	}
+	return (-1);
+}
