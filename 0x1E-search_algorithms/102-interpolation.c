331d5c58da2078b7ab235afe6b6cd6398186e65e
AMGHNOUSS <redouane.amghnouss@edu.uiz.ac.ma>
2024-03-12 15:39:26 +0000

    Create File interpolartion.c

---
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -0,0 +1,18 @@
+#include "search_algos.h"
+#include <math.h>
+
+/**
+ * interpolation_search - searches for a value in an array of
+ * integers using the Jump search algorithm
+ *
+ * @array: input array
+ * @size: size of the array
+ * @value: value to search in
+ * Return: index of the number
+ */
+int interpolation_search(int *array, size_t size, int value)
+{
+
+	if (array == NULL || size == 0)
+		return (-1);
+}
