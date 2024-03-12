60070d504ace009727259b8240c9aa5abb148836
AMGHNOUSS <redouane.amghnouss@edu.uiz.ac.ma>
2024-03-12 15:28:59 +0000

    Create File 100-jump.c

---
+++ 0x1E-search_algorithms/100-jump.c
@@ -0,0 +1,34 @@
+#include "search_algos.h"
+
+/**
+ * jump_search - search for a value in an array using the Jump search
+ * @array: list of integrs.
+ * @size: lenght of list.
+ * @value: value that i want to found.
+ * Return: Integer or NULL
+ */
+int jump_search(int *array, size_t size, int value)
+{
+	size_t i, step, jump;
+
+	if (array == NULL || size == 0)
+		return (-1);
+
+	step = sqrt(size);
+
+	for (i = jump = 0; jump < size && array[jump] < value;)
+	{
+		printf("Value checked array [%ld] = [%d]\n", jump, array[jump]);
+		i = jump;
+		jump += step;
+	}
+
+	printf("Value found between indexes [%ld] and [%ld]\n", i, jump);
+
+	jump = jump > size - 1 ? jump : size - 1;
+	for (; i < jump && array[i] < value; i++)
+		printf("Value checked array [%ld] = [%d]\n", i, array[i]);
+	printf("Value checked array [%ld] = [%d]\n", i, array[i]);
+
+	return (array[i] == value ? (int)i : -1);
+}
