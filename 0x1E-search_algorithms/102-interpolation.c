bfb963d7931080a1e9b2b7541e4d1c1d92a8b738
AMGHNOUSS <redouane.amghnouss@edu.uiz.ac.ma>
2024-03-16 23:51:42 +0000

    Create File 102-interpolation.c

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -12,7 +12,29 @@
  */
 int interpolation_search(int *array, size_t size, int value)
 {
+	size_t pos, low, high;
 
 	if (array == NULL || size == 0)
 		return (-1);
+	for (low = 0, (high = size - 1); high >= low;)
+	{
+		pos = low + (((double)(high - low) / (array[high] - array[low]))
+				* (value - array[low]));
+		if (pos < size)
+		{
+			printf("Value checked array [%ld] = [%d]\n", pos, array[pos]);
+		}
+		else
+		{
+			printf("Value checked array [%ld] is out of range\n", pos);
+			break;
+		}
+		if (array[pos] == value)
+			return (pos);
+		if (array[pos] > value)
+			high = pos - 1;
+		else
+			low = pos + 1;
+	}
+	return (-1);
 }
